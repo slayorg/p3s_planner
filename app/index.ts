5fcdaea814aad7958e51b5efd90212974439b040
slayorg <xmenmatt@gmail.com>
2022-01-21 02:16:13 -0500

    Fixed bug in logic

--- app/index.ts
+++ app/index.ts
@@ -12,7 +12,7 @@ if (hot) {
 const earthlyStar = 10;
 
 const stage = new Konva.Stage({
-    container: "app",
+    container: "container",
     width: 500,
     height: 500
 });
@@ -175,7 +175,7 @@ function getDistance(a: {x:number, y: number}, b: {x: number, y: number}){
 }
 
 function updateCones(){
-    const targets = [...players];
+    let targets = [...players];
     bossCones.forEach(cone => {
         let target: Konva.Circle = targets[0];
         let dist = getDistance(target.position(), boss.position());
@@ -196,6 +196,8 @@ function updateCones(){
         const angle = Math.atan2(target.y(), target.x()) - Math.atan2(boss.y(), boss.x());
         cone.rotation((angle * 180 / Math.PI)-90);
     });
+
+    targets = [...players];
     
     tornadoCones.forEach((cone, ci )=> {
         let target: Konva.Circle = targets[0];
@@ -225,6 +227,13 @@ function updateCones(){
     console.log(JSON.stringify(players.map(p => ({x:p.x(), y:p.y()}))));
 }
 
+
+const strats = {
+    elmo: [{"x":-2.200000000000001,"y":3.959999999999992},{"x":1.9755339059327315,"y":3.8555339059327345},{"x":-2.320000000000001,"y":-6.040000000000003},{"x":-2.744466094067267,"y":-4.855533905932732},{"x":-2.7200000000000024,"y":-6.88},{"x":-1.7355339059327357,"y":-5.095533905932734},{"x":2.7599999999999962,"y":-7.68},{"x":-3.6555339059327387,"y":-6.104466094067264}],
+    braindead:[{"x":-0.120000000000001,"y":2.8799999999999883},{"x":-0.1444660940672695,"y":2.5755339059327316},{"x":-3.240000000000002,"y":-7.8000000000000025},{"x":3.8555339059327336,"y":-3.4955339059327324},{"x":3.2799999999999976,"y":-3.08},{"x":2.824466094067269,"y":-2.6955339059327343},{"x":5.919999999999993,"y":6.760000000000002},{"x":-5.97553390593274,"y":6.135533905932735}],
+    fiesta: [{"x":-3.3599999999999994,"y":2.519999999999998},{"x":3.4955339059327346,"y":2.895533905932737},{"x":-6.2,"y":6.52},{"x":6.89553390593274,"y":6.3844660940672675},{"x":-2.6400000000000015,"y":-8.84},{"x":0.024466094067264077,"y":1.664466094067266},{"x":-2.3200000000000003,"y":-1.4800000000000004},{"x":1.94446609406726,"y":-1.424466094067264}]
+};
+
 const data = [{"x":-3.3599999999999994,"y":2.519999999999998},{"x":3.4955339059327346,"y":2.895533905932737},{"x":-6.2,"y":6.52},{"x":6.89553390593274,"y":6.3844660940672675},{"x":-2.6400000000000015,"y":-8.84},{"x":0.024466094067264077,"y":1.664466094067266},{"x":-2.3200000000000003,"y":-1.4800000000000004},{"x":1.94446609406726,"y":-1.424466094067264}];
 
 data.forEach((d, i) => {
@@ -237,3 +246,17 @@ stage.add(layer);
 
 
 updateCones();
+
+const buttonContainer = document.getElementById("buttons");
+
+for(const [key, value] of Object.entries(strats)){
+    const btn = document.createElement("button");
+    btn.innerText = key;
+    btn.addEventListener("click", () => {
+        value.forEach((d, i) => {
+            players[i].position(d);
+        });
+        updateCones();
+    });
+    buttonContainer?.appendChild(btn);
+}

9827b47499222c8c570e87dfda40f87081a10ef1
slayorg <xmenmatt@gmail.com>
2022-01-21 18:45:01 -0500

    Updated project architecture

---
+++ app/arena.ts
@@ -0,0 +1,202 @@
+import Konva from "konva";
+
+interface ArenaObjectConfig{
+    opacity?: number;
+    fill?: string;
+    draggable?: boolean;
+}
+
+function getDistance(a: ArenaObject, b: ArenaObject){
+    return Math.sqrt(((a.x-b.x)**2) + ((a.y-b.y)**2));
+}
+
+abstract class ArenaObject{
+    x: number = 0;
+    y: number = 0;
+    scale: number = 1;
+    tags: string[] = [];
+
+    draggable = false;
+
+    abstract shape: Konva.Shape;
+
+    constructor(tags?: string[]){
+        if(tags){
+            this.tags = tags;
+        }
+    }
+
+    setPosition(x: number, y: number){
+        this.x = Math.round(x*100)/100;
+        this.y = Math.round(y*100)/100;
+        this.shape.position({
+            x: this.x * this.scale,
+            y: this.y * this.scale
+        });
+    }
+
+    setScale(scale: number){
+        this.scale = scale;
+        this.setPosition(this.x, this.y);
+    }
+
+    setTags(tags?:string[]){
+        if(tags){
+            this.tags = tags;
+        }
+    }
+
+    inMarker(obj: ArenaObject){
+        return false;
+    }
+
+    dragEnd = () => {};
+
+    setDraggable(){
+        this.draggable = true;
+        this.shape.draggable(true);
+        this.shape.on("dragend", () => {
+            this.setPosition(
+                this.shape.x() / this.scale,
+                this.shape.y() / this.scale
+            );
+            this.dragEnd();
+        });
+    }
+}
+
+export class CircleMarker extends ArenaObject{
+    shape: Konva.Circle;
+    radius: number;
+
+    constructor( radius: number, x: number, y: number, config: ArenaObjectConfig, tags?: string[]){
+        super(tags);
+        this.radius = radius;
+        this.shape = new Konva.Circle({
+            ...config,
+            radius: radius * this.scale
+        });
+        this.setPosition(x, y);
+        if(config.draggable){
+            this.setDraggable();
+        }
+    }
+
+    setScale(scale: number): void {
+        super.setScale(scale);
+        this.shape.radius(this.radius * this.scale);
+    }
+
+    inMarker(obj: ArenaObject): boolean{
+        return getDistance(obj, this) <= this.radius
+    }
+}
+
+export class ConeMarker extends ArenaObject{
+    shape: Konva.Line;
+    coneAngle: number;
+    size: number;
+    angle: number;
+
+    constructor(x: number, y: number, coneAngle: number, size: number, config: ArenaObjectConfig, tags?: string[]){
+        super(tags);
+        this.size = size;
+        this.coneAngle = coneAngle;
+        this.angle = 0;
+        this.shape = new Konva.Line({
+            ...config,
+            closed: true,
+        });
+        this.setPosition(x, y);
+    }
+
+    setScale(scale: number): void {
+        super.setScale(scale);
+        this.shape.points([
+            Math.sin(this.coneAngle/2) * (this.size*2*this.scale),
+            Math.cos(this.coneAngle/2) * (this.size*2*this.scale),
+            0, 0,
+            Math.sin(-this.coneAngle/2) * (this.size*2*this.scale),
+            Math.cos(-this.coneAngle/2) * (this.size*2*this.scale)
+        ]);
+    }
+
+    pointAt(target: ArenaObject){
+        const angle = Math.atan2(this.y - target.y, this.x - target.x);
+        this.setRotation(angle);
+    }
+
+    setRotation(angle: number){
+        this.angle = angle;
+        this.shape.rotation((angle * 180 / Math.PI)+90);
+    }
+
+    inMarker(obj: ArenaObject): boolean{
+        const angle = Math.atan2(this.y - obj.y, this.x - obj.x) - this.angle;
+        return angle < (this.coneAngle/2) && angle > -(this.coneAngle/2);
+    }
+}
+
+export class Arena{
+    scaleMultiplier: number = 1;
+    stageWidth: number = 20;
+
+    group: Konva.Group;
+
+    arenaObjects: ArenaObject[] = [];
+    arenaObjectTags: Record<string, ArenaObject[]> = {};
+
+    constructor(){
+        this.group = new Konva.Group();
+    }
+
+    addObject(arenaObject: ArenaObject){
+        arenaObject.setScale(this.scaleMultiplier);
+        this.group.add(arenaObject.shape);
+        this.arenaObjects.push(arenaObject);
+
+        arenaObject.tags.forEach(tag => {
+            if(!this.arenaObjectTags[tag]){
+                this.arenaObjectTags[tag] = [];
+            }
+            this.arenaObjectTags[tag].push(arenaObject);
+        });
+
+        if(arenaObject.draggable){
+            arenaObject.dragEnd = () => {
+                this.onUpdate();
+            }
+        }
+    }
+
+    getByTag(tags: string[], count: number = -1){
+        const results: ArenaObject[] = [];
+        for(let i=0; i<tags.length; i++){
+            const tag = tags[i];
+            if(this.arenaObjectTags[tag]){
+                results.push(...this.arenaObjectTags[tag]);
+            }
+        }
+        return results.filter((item, pos, arr) => {
+            return arr.indexOf(item) === pos;
+        });
+    }
+
+    getClosest(target: ArenaObject, tags: string[]){
+        let objs = this.getByTag(tags);
+        objs = objs.sort((a, b) => {
+            const dist = getDistance(target, a) - getDistance(target, b);
+            return dist;
+        });
+        return objs;
+    }
+
+    setWidth(width: number){
+        this.scaleMultiplier = width / this.stageWidth;
+        this.arenaObjects.forEach(obj => {
+            obj.setScale(this.scaleMultiplier);
+        });
+    }
+
+    onUpdate = () => {};
+}
--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,6 @@
 import Konva from "konva";
-console.log("ready");
 
-const hot = (module as any)?.hot;
-if (hot) {
-  hot.dispose(() => {
-    window.location.reload();
-    throw "hotReload";
-  });
-}
-
-const earthlyStar = 10;
+import { Arena, CircleMarker, ConeMarker } from "./arena";
 
 const stage = new Konva.Stage({
     container: "container",
@@ -18,246 +9,191 @@ const stage = new Konva.Stage({
 });
 
 const layer = new Konva.Layer();
-
-const scale = stage.width()/(earthlyStar*2);
-
 const group = new Konva.Group({
     x: stage.width()/2,
-    y: stage.height()/2,
-    scale: {x: scale, y: scale}
+    y: stage.height()/2
 });
-
-const stageCircle = new Konva.Circle({ 
-    radius: earthlyStar,
-    fill: "grey"
-});
-
-const boss = new Konva.Circle({
-    radius: 3,
-    fill: "black",
-    opacity: 0.5
-});
-
-group.add(stageCircle);
-group.add(boss);
-
-
-const tornados: Konva.Circle[] = [];
-
-const tornadoCones: Konva.Line[] = [];
-
-const angleInterval = Math.PI*2/3;
-
-for(let i=0; i<3; i++){
-    const tornado = new Konva.Circle({
-        radius: 2,
-        fill: "orange",
-        x: Math.sin(Math.PI+ (angleInterval)*i) * (earthlyStar - 2),
-        y: Math.cos(Math.PI+ (angleInterval)*i) * (earthlyStar - 2),
-        opacity: 0.5
-    });
-    console.log("place tornado at ", tornado.position());
-    group.add(tornado);
-    tornados.push(tornado);
-
-    const leftAngle = Math.PI*2/12;
-    const rightAngle = -Math.PI*2/12;
-    const tornadoCone = new Konva.Line({
-        x: tornado.x(),
-        y: tornado.y(),
-        points: [
-            Math.sin(leftAngle) * (earthlyStar*2),
-            Math.cos(leftAngle) * (earthlyStar*2),
-            0, 0,
-            Math.sin(rightAngle) * (earthlyStar*2),
-            Math.cos(rightAngle) * (earthlyStar*2)
-        ],
-        fill: "orange",
-        closed: true,
-        opacity: 0.5
-    });
-    group.add(tornadoCone);
-    tornadoCones.push(tornadoCone);
-}
-
-const bossCones: Konva.Line[] = [];
-
-for(let i=0; i<3; i++){
-    const leftAngle = Math.PI*2/12;
-    const rightAngle = -Math.PI*2/12;
-    const line = new Konva.Line({
-        points: [
-            Math.sin(leftAngle) * (earthlyStar+2),
-            Math.cos(leftAngle) * (earthlyStar+2),
-            0, 0,
-            Math.sin(rightAngle) * (earthlyStar+2),
-            Math.cos(rightAngle) * (earthlyStar+2)
-        ],
-        fill: "orange",
-        closed: true,
-        opacity: 0.5
-    });
-    bossCones.push(line);
-    group.add(line);
-    console.log(line.points());
-}
-
-const tankCircles: Konva.Circle[] = [];
-
-for(let i=0; i<2; i++){
-    const circle = new Konva.Circle({
-        radius: 3,
-        fill: "orange",
-        opacity: 0.5
-    })
-    group.add(circle);
-    tankCircles.push(circle);
-}
-
-
-
-
-const tanks: Konva.Circle[] = [];
-
-for(let i=0; i<2; i++){
-    const tank = new Konva.Circle({
-        radius: 0.5,
-        fill: "blue",
-        listening: true,
-        draggable: true
-    });
-    group.add(tank);
-    tanks.push(tank);
-}
-
-const healers: Konva.Circle[] = [];
-
-for(let i=0; i<2; i++){
-    const healer = new Konva.Circle({
-        radius: 0.5,
-        fill: "green",
-        listening: true,
-        draggable: true
-    });
-    group.add(healer);
-    healers.push(healer);
-}
-
-const damages: Konva.Circle[] = [];
-for(let i=0; i<4; i++){
-    const dps = new Konva.Circle({
-        radius: 0.5,
-        fill: "red",
-        listening: true,
-        draggable: true
-    });
-   
-    group.add(dps);
-    damages.push(dps);
-}
-
-const players = [...tanks, ...healers, ...damages];
-
-players.forEach((p, i)=> {
-    const angle = ((Math.PI*2)/players.length)*i;
-    p.position({
-        x: Math.sin(angle) * earthlyStar/2,
-        y: Math.cos(angle) * earthlyStar/2,
-    });
-
-    p.on("dragend", ev => {
-        updateCones();
-    });
-});
-
-function getDistance(a: {x:number, y: number}, b: {x: number, y: number}){
-    return Math.sqrt(((a.x-b.x)**2) + ((a.y-b.y)**2));
-}
-
-function updateCones(){
-    let targets = [...players];
-    bossCones.forEach(cone => {
-        let target: Konva.Circle = targets[0];
-        let dist = getDistance(target.position(), boss.position());
-
-        for(let i=1; i<targets.length; i++){
-            const t= targets[i];
-            const tDist = getDistance(t.position(), boss.position());
-            if(tDist < dist){
-                dist = tDist;
-                target = t;
-            }
-        }
-
-        const index = targets.indexOf(target);
-        if(index !== -1){
-            targets.splice(index, 1);
-        }
-        const angle = Math.atan2(target.y(), target.x()) - Math.atan2(boss.y(), boss.x());
-        cone.rotation((angle * 180 / Math.PI)-90);
-    });
-
-    
-    
-    tornadoCones.forEach((cone, ci )=> {
-        targets = [...players];
-        let target: Konva.Circle = targets[0];
-        let dist = getDistance(target.position(), cone.position());
-
-        for(let i=1; i<targets.length; i++){
-            const t= targets[i];
-            const tDist = getDistance(t.position(), cone.position());
-            if(tDist < dist){
-                dist = tDist;
-                target = t;
-            }
-        }
-        const index = targets.indexOf(target);
-        if(index !== -1){
-            targets.splice(index, 1);
-        }
-
-        const angle = Math.atan2(cone.y() - target.y(), cone.x() - target.x());
-        cone.rotation((angle * 180 / Math.PI)+90);
-    });
-
-    tanks.forEach((t, i) => {
-        tankCircles[i].position(t.position());
-    });
-
-    console.log(JSON.stringify(players.map(p => ({x:p.x(), y:p.y()}))));
-}
-
-
-const strats = {
-    elmo: [{"x":-2.200000000000001,"y":3.959999999999992},{"x":1.9755339059327315,"y":3.8555339059327345},{"x":-2.320000000000001,"y":-6.040000000000003},{"x":-2.744466094067267,"y":-4.855533905932732},{"x":-2.7200000000000024,"y":-6.88},{"x":-1.7355339059327357,"y":-5.095533905932734},{"x":2.7599999999999962,"y":-7.68},{"x":-3.6555339059327387,"y":-6.104466094067264}],
-    braindead:[{"x":-0.120000000000001,"y":2.8799999999999883},{"x":-0.1444660940672695,"y":2.5755339059327316},{"x":-3.240000000000002,"y":-7.8000000000000025},{"x":3.8555339059327336,"y":-3.4955339059327324},{"x":3.2799999999999976,"y":-3.08},{"x":2.824466094067269,"y":-2.6955339059327343},{"x":5.919999999999993,"y":6.760000000000002},{"x":-5.97553390593274,"y":6.135533905932735}],
-    fiesta: [{"x":-3.3599999999999994,"y":2.519999999999998},{"x":3.4955339059327346,"y":2.895533905932737},{"x":-6.2,"y":6.52},{"x":6.89553390593274,"y":6.3844660940672675},{"x":-2.6400000000000015,"y":-8.84},{"x":0.024466094067264077,"y":1.664466094067266},{"x":-2.3200000000000003,"y":-1.4800000000000004},{"x":1.94446609406726,"y":-1.424466094067264}]
-};
-
-const data = [{"x":-3.3599999999999994,"y":2.519999999999998},{"x":3.4955339059327346,"y":2.895533905932737},{"x":-6.2,"y":6.52},{"x":6.89553390593274,"y":6.3844660940672675},{"x":-2.6400000000000015,"y":-8.84},{"x":0.024466094067264077,"y":1.664466094067266},{"x":-2.3200000000000003,"y":-1.4800000000000004},{"x":1.94446609406726,"y":-1.424466094067264}];
-
-data.forEach((d, i) => {
-    players[i].position(d);
-});
-
-
 layer.add(group);
+
+const arena = new Arena();
+
+group.add(arena.group);
 stage.add(layer);
 
+arena.setWidth(stage.width());
 
-updateCones();
+arena.addObject( new CircleMarker(10, 0, 0, {fill: "grey"}));
+arena.addObject( new CircleMarker(3, 0, 0, { fill: "black", opacity: 0.5 }, ["boss"]));
 
-const buttonContainer = document.getElementById("buttons");
+for(let i=0; i<3; i++){
+    const angle = Math.PI*2/3*i;
+    arena.addObject(new CircleMarker(
+        2,
+        Math.sin(Math.PI+ angle) * ((arena.stageWidth/2) - 2),
+        Math.cos(Math.PI+ angle) * ((arena.stageWidth/2) - 2),
+        {
+            fill: "red",
+            opacity: 1
+        },
+        ["tornado", "damage", "kill"]
+    ));
+    console.log("add tornaod");
+}
 
-for(const [key, value] of Object.entries(strats)){
-    const btn = document.createElement("button");
-    btn.innerText = key;
-    btn.addEventListener("click", () => {
-        value.forEach((d, i) => {
-            players[i].position(d);
-        });
-        updateCones();
+arena.getByTag(["boss"]).forEach(boss => {
+    for(let i=0; i<3; i++){
+        arena.addObject(new ConeMarker(boss.x, boss.y, Math.PI*2/6, arena.stageWidth*2,
+            {
+                fill: "orange",
+                opacity: 0.5
+            },
+            ["bosscone", "damage"]
+        ));
+    }
+
+});
+
+arena.getByTag(["tornado"]).forEach(tornado => {
+    arena.addObject(new ConeMarker(tornado.x, tornado.y, Math.PI*2/6, arena.stageWidth*2,
+        {fill: "orange", opacity: 0.5},
+        ["tornadocone", "damage"]
+    ));
+});
+
+for(let i=0; i<2; i++){
+    arena.addObject(new CircleMarker(3, 0, 0, {
+        fill: "orange",
+        opacity: 0.5
+    }, ["tanktether", "damage"]));
+}
+
+for(let i=0; i<2; i++){
+    arena.addObject(new CircleMarker(0.5, 0, 0, {fill: "blue", draggable: true}, ["player", "tank"]));
+}
+for(let i=0; i<2; i++){
+    arena.addObject(new CircleMarker(0.5, 5, 5, {fill: "green", draggable: true}, ["player", "healer"]));
+}
+for(let i=0; i<4; i++){
+    arena.addObject(new CircleMarker(0.5, 5, 0, {fill: "red",  draggable: true}, ["player", "dps"]));
+}
+
+arena.onUpdate = () => {
+    const boss = arena.getByTag(["boss"])[0];
+    const bossTargets = arena.getClosest(boss, ["player"]);
+    arena.getByTag(["bosscone"]).forEach((cone, index) => {
+        const target = bossTargets[index];
+        if(target){
+            (cone as ConeMarker).pointAt(target);
+        }
     });
-    buttonContainer?.appendChild(btn);
+
+    arena.getByTag(["tornadocone"]).forEach((cone, index) => {
+        const target = arena.getClosest(cone, ["player"])[0];
+        if(target){
+            (cone as ConeMarker).pointAt(target);
+        }
+    });
+
+    const tanks = arena.getByTag(["tank"]);
+    arena.getByTag(["tanktether"]).forEach((aoe, index) => {
+        aoe.setPosition(tanks[index].x, tanks[index].y);
+    });
+
+
+    const damageMarkers = arena.getByTag(["damage"]);
+    const players = arena.getByTag(["player"]) as CircleMarker[];
+    const playerHits: number[] = new Array(players.length).fill(0);
+    damageMarkers.forEach(aoe => {
+        players.forEach((player, pindex) => {
+            if(aoe.inMarker(player)){
+                playerHits[pindex]++;
+            }
+        });
+    });
+    playerHits.forEach((hitCount, index) => {
+        const player = players[index];
+        if(hitCount > 1){
+            player.shape.stroke("magenta");
+        } else {
+            player.shape.stroke("none");
+        }
+    });
+}
+
+arena.onUpdate();
+
+function loadData(data: number[]){
+    const players = arena.getByTag(["player"]);
+    if(data && data.length === players.length*2){
+        for(let i=0; i<players.length; i++){
+            players[i].setPosition(data[i*2], data[i*2+1]);
+        }
+        arena.onUpdate();
+    }
+}
+
+(() => {
+    const strats = {
+        "Elmo": [-2.2,3.96,1.98,3.86,-2.32,-6.04,-2.74,-4.86,-2.72,-6.88,-1.74,-5.1,2.76,-7.68,-3.66,-6.1],
+        "Braindead": [-0.12,2.88,-0.14,2.58,-3.24,-7.8,3.86,-3.5,3.28,-3.08,2.82,-2.7,5.92,6.76,-5.98,6.14],
+        "Fiesta": [-3.36,2.52,3.5,2.9,-6.2,6.52,6.9,6.38,-2.64,-8.84,0.02,1.66,-2.32,-1.48,1.94,-1.42],
+        "Full Uptime": [-3.48,-1.24,3.34,-0.7,0.96,3.88,0.9,3.9,0.8,3.68,0.82,4.06,0.96,3.6,0.86,3.66],
+        "Partial Uptime": [0.12,2.28,0.18,2.46,3.64,2,8.5,-4.22,3.4,1.68,-3.18,1.38,5.68,1.04,-3.02,5.46],
+        "Kill Ri": [-2.04,3.48,2.42,3.34,-1.48,-3.32,2.1,-2.7,7.44,1.44,-2.46,-1.3,1.24,-9.72,-7.14,0.82],
+        "Slay's Uptime": [-1.8,2.64,1.82,2.86,-3.68,-3.88,7.1,1.62,2.6,-0.84,-0.42,-2.26,1.4,-2.12,-2.58,6.78]
+    };
+    
+    const buttonContainer = document.getElementById("buttons");
+    
+    const players = arena.getByTag(["player"]);
+    for(const [key, value] of Object.entries(strats)){
+        const btn = document.createElement("button");
+        btn.innerText = key;
+        btn.addEventListener("click", () => {
+            loadData(value);
+        });
+        buttonContainer?.appendChild(btn);
+    }
+
+    const exportContainer = document.createElement("div");
+    const textarea = document.createElement("textarea");
+    const importBtn = document.createElement("button");
+    const exportBtn = document.createElement("button");
+
+    importBtn.innerText = "Import";
+    exportBtn.innerText = "Export";
+
+    exportContainer.appendChild(textarea);
+    exportContainer.appendChild(importBtn);
+    exportContainer.appendChild(exportBtn);
+
+    importBtn.addEventListener("click", () => {
+        let data: number[] = [];
+        try{
+            data = JSON.parse(textarea.value || "");
+        }catch(e){
+            console.error("invalid data");
+        }
+        loadData(data);
+    });
+
+    exportBtn.addEventListener("click", () => {
+        const data: number[] = [];
+        players.forEach(p => {
+            data.push(p.x, p.y);
+        });
+        textarea.textContent = JSON.stringify(data);
+    });
+
+    buttonContainer?.appendChild(exportContainer);
+
+    loadData(strats.Fiesta);
+})();
+
+const hot = (module as any)?.hot;
+if (hot) {
+  hot.dispose(() => {
+    window.location.reload();
+    throw "hotReload";
+  });
 }
